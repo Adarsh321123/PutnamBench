82cb2b375c412582c35e759f991d3e6aac977815
Adarsh321123 <adarshk1234567@gmail.com>
2024-08-09 21:34:07 -0700

    refactor into real lean4 repo

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,9 @@
 import Lake
 open Lake DSL
 
-package «putnam» where
-  -- add package configuration options here
+package «PutnamBench» where
+
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.7.0"
 
 @[default_target]
-lean_lib «putnam» where
-  globs := #[.submodules `src]
+lean_lib «PutnamBench» where
